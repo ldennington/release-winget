6469568c70ed808c024760c7ecbf14dccb5f42b1
Lessley Dennington <ledennin@microsoft.com>
2021-04-23 11:04:24 -0700

    Fixing errors

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import github from '@actions/github';
-import core from '@actions/core';
+import github from '@actions/github'
+import core from '@actions/core'
 
 async function run(): Promise<void> {
   try {
@@ -7,23 +7,23 @@ async function run(): Promise<void> {
     // The YML workflow will need to set myToken with the GitHub Secret Token
     // myToken: ${{ secrets.GITHUB_TOKEN }}
     // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
-    const myToken = core.getInput('WINGET_TOKEN');
+    const myToken = core.getInput('WINGET_TOKEN')
 
     const octokit = github.getOctokit(myToken)
 
     // You can also pass in additional options as a second parameter to getOctokit
     // const octokit = github.getOctokit(myToken, {userAgent: "MyActionVersion1"});
 
-    const { data: pullRequest } = await octokit.pulls.get({
-        owner: 'octokit',
-        repo: 'rest.js',
-        pull_number: 123,
-        mediaType: {
-          format: 'diff'
-        }
-    });
+    const {data: pullRequest} = await octokit.pulls.get({
+      owner: 'octokit',
+      repo: 'rest.js',
+      pull_number: 123,
+      mediaType: {
+        format: 'diff'
+      }
+    })
 
-    console.log(pullRequest);
+    console.log(pullRequest)
   } catch (error) {
     core.setFailed(error.message)
   }
