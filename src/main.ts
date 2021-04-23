3a45f487c0a8ad951c04ac4bce9c868b8df2d5a9
Lessley Dennington <ledennin@microsoft.com>
2021-04-23 10:45:00 -0700

    Trying out octokit

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,29 @@
-import * as core from '@actions/core'
+import github from '@actions/github';
+import core from '@actions/core';
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    // This should be a token with access to your repository scoped in as a secret.
+    // The YML workflow will need to set myToken with the GitHub Secret Token
+    // myToken: ${{ secrets.GITHUB_TOKEN }}
+    // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
+    const myToken = core.getInput('WINGET_TOKEN');
 
-    core.debug(new Date().toTimeString())
-    core.debug(new Date().toTimeString())
+    const octokit = github.getOctokit(myToken)
 
-    core.setOutput('time', new Date().toTimeString())
+    // You can also pass in additional options as a second parameter to getOctokit
+    // const octokit = github.getOctokit(myToken, {userAgent: "MyActionVersion1"});
+
+    const { data: pullRequest } = await octokit.pulls.get({
+        owner: 'octokit',
+        repo: 'rest.js',
+        pull_number: 123,
+        mediaType: {
+          format: 'diff'
+        }
+    });
+
+    console.log(pullRequest);
   } catch (error) {
     core.setFailed(error.message)
   }
